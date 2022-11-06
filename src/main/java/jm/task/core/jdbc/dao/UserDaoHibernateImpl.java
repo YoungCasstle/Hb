0d4609582612f00db6d4924b777d076a1564df37
YoungCasstle <kramer337@mail.ru>
2022-11-06 16:46:24 +0300

    peredelanniy hb

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -88,4 +88,5 @@ public class UserDaoHibernateImpl implements UserDao {
         }
     }
 }
+// Попытался исправить весь код
 
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -41,3 +41,5 @@ public class UserServiceImpl implements UserService {
         userDao.cleanUsersTable();
     }
 }
+
+//
